3642a0f05d69d470ca4fabc4c9fd89928ce299ec
Marvin Müller <marvin.mueller@code.berlin>
2025-03-15 11:22:01 +0100

    Add secret message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,8 +59,12 @@ export default async function Home() {
               <SignInSignOutButton />
             </div>
           </div>
-
           {session?.user && <LatestPost />}
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-center text-2xl text-white">
+            {session && api.post.getSecretMessage()}
+          </p>
         </div>
       </main>
     </HydrateClient>
