5d6ff7ed65a74bd238bfa8a5f412ae7e80f2088c
Marvin Müller <marvin.mueller@code.berlin>
2025-03-15 12:17:58 +0100

    Add account deletion page

--- src/app/_components/forms/auth/delete-account-form.tsx
+++ src/app/_components/forms/auth/delete-account-form.tsx
@@ -1,21 +1,57 @@
 "use client";
-import { cn } from "~/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "~/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
-import { useRouter } from "next/navigation";
-import { DeleteAccountDialog } from "../../dialogs/auth/delete-account-dialog";
+import { CardContent } from "~/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { authClient } from "~/lib/auth-client";
+import { cn } from "~/lib/utils";
+
+const formSchema = z.object({
+  password: z.string(),
+});
 
 export function DeleteAccountForm({
   className,
   ...props
-}: React.ComponentPropsWithoutRef<"div">) {
-  const router = useRouter();
+}: {
+  className?: string;
+  props?: React.ComponentPropsWithoutRef<"div">;
+}) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const { error } = await authClient.deleteUser({
+      password: values.password,
+    });
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Account deleted successfully");
+    }
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
@@ -26,18 +62,45 @@ export function DeleteAccountForm({
         <CardContent>
           <div className="grid gap-6">
             <div className="grid gap-6">
-              <DeleteAccountDialog>
-                <Button type="submit" className="w-full" variant="destructive">
-                  Delete your account :(
-                </Button>
-              </DeleteAccountDialog>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <div className="flex flex-col gap-2">
+                    <FormField
+                      control={form.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              id="password"
+                              type="password"
+                              placeholder="********"
+                              required
+                            />
+                          </FormControl>
+                          <FormDescription>
+                            Enter your password to delete your account
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button
+                      type="submit"
+                      variant="destructive"
+                      className="w-full"
+                    >
+                      Delete account
+                    </Button>
+                  </div>
+                </form>
+              </Form>
             </div>
             <div className="text-center text-sm">
               Changed your mind ?{" "}
-              <a
-                className="underline underline-offset-4"
-                onClick={() => router.back()}
-              >
+              <a className="underline underline-offset-4" href="/">
                 Go back
               </a>
             </div>
--- src/app/auth/(authorized)/delete/page.tsx
+++ src/app/auth/(authorized)/delete/page.tsx
@@ -1,5 +1,6 @@
 import { GalleryVerticalEnd } from "lucide-react";
 import { DeleteAccountForm } from "~/app/_components/forms/auth/delete-account-form";
+import { Button } from "~/components/ui/button";
 
 export default function LoginPage() {
   return (
