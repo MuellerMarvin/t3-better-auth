acf999e5257d69f316705c86d64960e6de7a0b17
Marvin Müller <marvin.mueller@code.berlin>
2025-03-15 11:49:51 +0100

    Change title and description of account deletion form

--- src/app/_components/forms/auth/delete-account-form.tsx
+++ src/app/_components/forms/auth/delete-account-form.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { cn } from "~/lib/utils";
 import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import { useRouter } from "next/navigation";
 import { DeleteAccountDialog } from "../../dialogs/auth/delete-account-dialog";
 
@@ -14,14 +20,15 @@ export function DeleteAccountForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <CardTitle className="text-xl">Reset your password</CardTitle>
+          <CardTitle className="text-xl">Account Deletion</CardTitle>
+          <CardDescription>Delete your account irreversibly.</CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid gap-6">
             <div className="grid gap-6">
               <DeleteAccountDialog>
                 <Button type="submit" className="w-full" variant="destructive">
-                  Delete Your Account
+                  Delete your account
                 </Button>
               </DeleteAccountDialog>
             </div>
