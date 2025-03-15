7a103cc4ac0525d9b916a2ae662ddc0a34aa1294
Marvin Müller <marvin.mueller@code.berlin>
2025-03-15 12:22:09 +0100

    Use layout for auth

--- src/app/auth/(unauthorized)/reset/page.tsx
+++ src/app/auth/(unauthorized)/reset/page.tsx
@@ -4,17 +4,5 @@ import { LoginForm } from "~/app/_components/forms/auth/login-form";
 import { PasswordResetForm } from "~/app/_components/forms/auth/password-reset-form";
 
 export default function LoginPage() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-md flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          Summercamp
-        </a>
-        <PasswordResetForm />
-      </div>
-    </div>
-  );
+  return <PasswordResetForm />;
 }
