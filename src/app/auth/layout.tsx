52669bf3a762a245cb1a0b0d1c428ae3a74a1748
Marvin Müller <marvin.mueller@code.berlin>
2025-09-29 10:00:59 +0200

    Wrap auth section into suspense boundary (useSearchParams for /redirect)

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import { GalleryVerticalEnd } from "lucide-react";
+import { Suspense } from "react";
 
 export default function AuthLayout({
   children,
@@ -6,15 +7,15 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-md flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <div className="bg-primary text-primary-foreground flex h-6 w-6 items-center justify-center rounded-md">
             <GalleryVerticalEnd className="size-4" />
           </div>
           T3 Better Auth Template
         </a>
-        {children}
+        <Suspense>{children}</Suspense>
       </div>
     </div>
   );
