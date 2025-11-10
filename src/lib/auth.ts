45d749ee9f7e177bf1f3f57deae4db17d30f482b
Marvin Müller <marvin.mueller@code.berlin>
2025-11-10 06:36:26 +0100

    Pass auth schema directly

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
+import * as authSchema from "~/db/auth-schema";
 import { env } from "~/env";
 import { db } from "~/lib/server/db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: authSchema,
   }),
   socialProviders: {
     google: {
